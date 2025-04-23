65f986a690203138abe6ec48d7f552a21b52b4cc
Suraj Bhosale <bhosalesuraj074@gmail.com>
2025-04-24 00:57:40 +0530

    basic
    basic

--- com/core/Arrays/ArrayDemo.java
+++ com/core/Arrays/ArrayDemo.java
@@ -9,23 +9,22 @@ public class ArrayDemo {
         for(int i : arr){
             System.out.print(i+" ");
         }
-
+        
         System.out.println("\nAfter sorting");
+        
         // 1. sorting the array by sort() method
         Arrays.sort(arr);
-
         for(int i : arr){
             System.out.print(i+" ");
         }
-
+        
         // 2. sorting the array by parallelSort() by passing fromIndex & toIndex
-
         /*Arrays.parallelSort(arr, 4, arr.length-1);
         for(int i : arr){
             System.out.print(i+" ");
         }*/
-
-      /* 3. searching the element from the array by using Binary search, it accepts two values first array and search
+        
+        /* 3. searching the element from the array by using Binary search, it accepts two values first array and search
         element as key i.e (array, key), then it will return the index of occurrence*/
 
         System.out.println("\nFinding the element: " + Arrays.binarySearch(arr, 234));
