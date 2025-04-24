539403d597877d276ade0292ac620af01e7ffe79
Suraj Bhosale <bhosalesuraj074@gmail.com>
2025-04-24 10:21:29 +0530

    added the features like delete by id and delete all
    added the features like delete by id and delete all

--- com/core/booking/FlightBookingService.java
+++ com/core/booking/FlightBookingService.java
@@ -3,10 +3,11 @@ package com.core.booking;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class FightBookingService {
+public class FlightBookingService {
     public static void main(String[] args) {
         int choice =0;
         int flightUpdateId= 0;
+        int flightRemoveId= 0;
         FlightBooking flights[] = {
                 new FlightBooking(101, "IndiGo", "Mumbai", "Delhi", "Economy"),
                 new FlightBooking(102, "Air India", "Bangalore", "Hyderabad", "Business"),
@@ -18,6 +19,7 @@ public class FightBookingService {
 
         Scanner sc = new Scanner(System.in);
         do {
+
             System.out.println("**************************************************************************************************************");
             System.out.println("1: Display all the flight\n2: update the flight record\n3.delete the record by Id\n4.delete the all records\n5.exit");
             System.out.println("------------------------------------");
@@ -26,35 +28,58 @@ public class FightBookingService {
             System.out.println("-------------------------------------");
             switch (choice) {
 
+
+                 /*there in this budge code we don't remove the element from the just assign null which record we want to delete,
+                 due to array are the fixed size once its initialized then , we can't able to modify the array size
+                */
                 case 1:
                     if (flights.length>=0){
-                    FightBookingService.DisplayRecords(flights);
-                    }
-                    else {
+                    FlightBookingService.DisplayRecords(flights);
+                    } else {
                         System.out.println("No record found");
                     }
-
                     break;
 
 
                 case 2:
                     System.out.println("Please enter the flightId of record which you want update");
                     flightUpdateId = sc.nextInt();
+                    boolean flag=true;
                     for (FlightBooking flight : flights){
 
                         if(flight !=null && flight.flightId == flightUpdateId){
-                            FightBookingService.updateRecord(flight);
+                            FlightBookingService.updateRecord(flight);
+                            flag = false;
                         }
+
+                    }
+                    if (flag){
+                        System.out.println("entered record not available ");
                     }
 
                     break;
 
                 case 3:
-                    System.out.println("Deleting the by id");
+                    System.out.println("please flight id which record want to remove");
+                    flightRemoveId = sc.nextInt();
+                    int result=  FlightBookingService.deleteFlightById(flightRemoveId, flights);
+                    if(result >=0){
+                        flights[result]=null;
+                        System.out.println("Record deleted successfully");
+                    }
+                    else {
+                        System.out.println("No such record found");
+                    }
                     break;
+
+
                 case 4 :
-                    System.out.println("Deleting all the records");
+
+                        Arrays.fill(flights, null);
+
+                    System.out.println("all the records deleted successfully");
                     break;
+
                 case 5:
                     System.exit(0);
                     System.out.println("exited");
@@ -94,8 +119,8 @@ public class FightBookingService {
         System.out.println("Enter the new  from where takeoff");
         flight.from = sc.nextLine();
 
-        System.out.println("Current to: "+ flight.to);
-        System.out.println("Enter the new to takeoff");
+        System.out.println("Current landing to: "+ flight.to);
+        System.out.println("Enter the new landing");
         flight.to = sc.nextLine();
 
         System.out.println("Current seatType: "+ flight.seatType);
@@ -104,6 +129,20 @@ public class FightBookingService {
         System.out.println("Hola! Record updated successfully");
 
 
+    }
+
+    // deleting the record by flight id
+    public static int deleteFlightById(int id , FlightBooking[] flights){
+        boolean isAvailable = false;
+        int counter=0;
+        for (FlightBooking flight: flights){
+            if (flight != null && flight.flightId == id){
+                isAvailable = true;
+                break;
+            }
+            counter++;
+        }
+        return isAvailable ? counter : -1;
     }
 
 
