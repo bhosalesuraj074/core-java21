3e82008e5b9ca35cd936d90cf98ab4e30c9b03ec
Suraj Bhosale <bhosalesuraj074@gmail.com>
2025-04-29 09:06:22 +0530

    static keyword

---
+++ com/core/statickeyword/StaticDemo.java
@@ -0,0 +1,27 @@
+package com.core.statickeyword;
+
+public class StaticDemo {
+    int a=10;
+    static int b=10;
+
+    // static block
+    static {
+        System.out.println("Static block");
+    }
+    // initialization block
+    {
+        System.out.println("Initialization block");
+    }
+   public  StaticDemo(){
+       System.out.println("Static Demo constructor");
+   }
+   public  static  void staticMethod(){
+       // System.out.println(a); - can't access the non-static mebers in the static blocks or method
+       System.out.println(b);
+   }
+
+    public static void main(String[] args) {
+    new StaticDemo();
+    StaticDemo.staticMethod();
+    }
+}
