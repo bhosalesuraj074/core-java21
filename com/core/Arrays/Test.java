c6d929cf32648ea7c29264318c2282de60499f94
Suraj Bhosale <bhosalesuraj074@gmail.com>
2025-04-29 09:05:45 +0530

    basic

--- com/core/Arrays/Test.java
+++ com/core/Arrays/Test.java
@@ -2,7 +2,13 @@ package com.core.Arrays;
 
 public class Test {
 
+    private Test(){
+        System.out.println("Default Constructor");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world");
+        new Test();
     }
+
 }
