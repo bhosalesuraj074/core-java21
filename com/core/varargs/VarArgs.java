ee15e5fb8d8e7370c98a485010f2f49cc3acbef4
Suraj Bhosale <bhosalesuraj074@gmail.com>
2025-04-29 10:09:51 +0530

    varArgs example
    varArgs example

---
+++ com/core/varargs/VarArgs.java
@@ -0,0 +1,33 @@
+package com.core.varargs;
+
+import java.util.Arrays;
+
+public class VarArgs {
+
+    public void varArgs(boolean ...varArgs){
+        System.out.println("Boolean value: "+ Arrays.toString(varArgs));
+    }
+
+    // 
+    public  void varArgs(int ...varArgs){
+        System.out.println("single int Parameter"+ Arrays.toString(varArgs));
+    }
+
+    // varArgs parameter must place at the end
+    public void varArgs(int a , int ...varArgs){
+        System.out.println("Values: "+ a);
+        System.out.println(Arrays.toString(varArgs));
+    }
+
+
+
+    public static void main(String[] args) {
+        VarArgs v = new VarArgs();
+        // Ambiguous method call. compiler got confused which should call
+        //v.varArgs(10,10,20,30,40);
+        v.varArgs(true, false, true, true, false);
+
+
+
+    }
+}
