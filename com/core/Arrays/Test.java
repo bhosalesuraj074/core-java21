beb98e051ccea0274993ce7a3df41fd4be0b0079
Suraj Bhosale <bhosalesuraj074@gmail.com>
2025-05-03 19:42:07 +0530

    Increasing the size of the array by using the Arrays.copyOf() method,
    Arrays.copyOf(datatype[] originalArray , int newSize );

--- com/core/Arrays/Test.java
+++ com/core/Arrays/Test.java
@@ -1,5 +1,8 @@
 package com.core.Arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Test {
 
     private Test(){
@@ -9,6 +12,14 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
         new Test();
+        int a[] = new int[3];
+        a[0]=10;
+        a[1]=20;
+        a[2]=30;
+        System.out.println("Array length: "+ a.length);
+        a= Arrays.copyOf(a, 5);
+        System.out.println("After updating the size of the array: "+a.length);
+
 
     }
 
