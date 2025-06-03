2c8fa245688e728c3b52fad377a1de69984d5bda
Suraj Bhosale <bhosalesuraj074@gmail.com>
2025-06-03 12:42:08 +0530

    Array List Collections
    The ArrayList class extends AbstractList and implements the List interface. ArrayList is a generic class that has this declaration:

---
+++ com/collection/ArrayListCollection.java
@@ -0,0 +1,22 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ArrayListCollection {
+    public static void main(String[] args) {
+        ArrayList<String> s = new ArrayList<>();
+        s.add("Suraj");
+        s.add("Sujal");
+        s.add("Akash");
+        s.add("Aniket");
+        System.out.println(s);
+
+
+
+
+
+
+    }
+}
